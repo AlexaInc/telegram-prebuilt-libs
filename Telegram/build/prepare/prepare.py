352fe0a1e5970baa8d50d3430925dacf71e2620d
AlexaInc <rasanjanahansaka2006@gmail.com>
2026-04-30 13:33:37 +0530

    Fix ffmpeg regex in python snippet without newline dependency

--- Telegram/build/prepare/prepare.py
+++ Telegram/build/prepare/prepare.py
@@ -1245,7 +1245,7 @@ winarm:
     SET "ARCH_PARAM=--arch=aarch64"
 win:
 depends:patches/build_ffmpeg_win.sh
-    python -c "data=open('../patches/build_ffmpeg_win.sh', 'rb').read().replace(b'\\r\\n', b'\\n'); data=data.replace(b'cd $FullScriptPath/../nv-codec-headers\\nmake PREFIX=\"$FullScriptPath/../local\" install\\n', b''); open('../patches/build_ffmpeg_win.sh', 'wb').write(b'#!/bin/bash\\nset -ex\\n' + data)"
+    python -c "data=open('../patches/build_ffmpeg_win.sh', 'rb').read().replace(b'\\r\\n', b'\\n'); data=data.replace(b'cd $FullScriptPath/../nv-codec-headers', b'# skipping headers').replace(b'make PREFIX=\"$FullScriptPath/../local\" install', b'# skipping install'); open('../patches/build_ffmpeg_win.sh', 'wb').write(b'#!/bin/bash\\nset -ex\\n' + data)"
     if not exist %THIRDPARTY_DIR%\\msys64\\tmp mkdir %THIRDPARTY_DIR%\\msys64\\tmp
     bash -c "mkdir -p /tmp && bash ../patches/build_ffmpeg_win.sh || (cat ffbuild/config.log && exit 1)"
 mac:
